01702fe020060c11f26e3c9a85fecc027d591441
KLEIN Jean-Daniel <kleinjeandaniel@gmail.com>
2023-12-02 16:32:42 +0100

    add responsive bar when hold

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ let totalBar = audioApp.querySelector(".bar-container");
 const progressContainer = audioApp.querySelector(".progress-container");
 let isOn = false;
 let currentIndex = 0;
-let mouseState;
+let x = 0;
+let mouseDown = false;
 
 let musicDisplay = (musicIndex) => {
   songAudio.src = `\/assets\/${allSongs[musicIndex].src}.mp3`;
@@ -38,6 +39,7 @@ let resetDisplay = () => {
   playPauseMusic();
 };
 
+// display minutes and second on span, first is timer, second is span ID
 let timeSet = (timer, target) => {
   let minutes = Math.floor(timer / 60);
   let seconds = Math.floor(timer % 60) >= 10 ? Math.floor(timer % 60) : "0" + Math.floor(timer % 60);
@@ -49,8 +51,8 @@ songAudio.addEventListener("loadeddata", (e) => {
 });
 
 songAudio.addEventListener("timeupdate", (e) => {
-  timeSet(e.target.currentTime, "current");
-  progressBar.style.width = `${(e.target.currentTime * 100) / e.target.duration}%`;
+  if (mouseDown === false) timeSet(e.target.currentTime, "current");
+  if (mouseDown === false) progressBar.style.width = `${(e.target.currentTime * 100) / e.target.duration}%`;
   if (e.target.currentTime == e.target.duration) {
     if (currentIndex < allSongs.length - 1) currentIndex++;
     else currentIndex = 0;
@@ -78,32 +80,34 @@ prvBtn.addEventListener("click", () => {
   resetDisplay();
 });
 
-let moveBar;
-//     = () => {
-//     totalBar.onmousemove = (e) => {
-//       console.log(e);
-//         progressBar.width = (e.offsetX * 100) / totalBar.clientWidth / 100;
-//         setTimeout("callEvent()", 1);
-// }
+// set mousedown on the progress bar
+totalBar.addEventListener("mousedown", () => (mouseDown = true));
 
-// totalBar.addEventListener("mousedown", (e) => {
-//   mouseState = "on";
-//   if (mouseState) {
-//     totalBar.onmousemove = (e) => {
-//       let data = e;
-//       console.log(data);
-//       console.log(totalBar.clientWidth);
-//       progressBar.style.width = `${(e.offsetX * 100) / totalBar.clientWidth}%`;
-//     };
-//   } else {
-//   }
-// });
-// Window.onmouseup = () => {
-//   clearInterval(moveBar);
-// };
+// change the bar progress when mouse is down
+document.addEventListener("mousemove", (e) => {
+  if (mouseDown === true) {
+    console.log((((e.clientX - totalBar.offsetLeft) / totalBar.offsetWidth) * 100 * songAudio.duration) / 100);
+    // check if mouse X is after the bar
+    if (((e.clientX - totalBar.offsetLeft) / totalBar.offsetWidth) * 100 > 100) {
+      progressBar.style.width = "100%";
+      timeSet(`${songAudio.duration}`, "current");
+      // check if mouse X is before the bar
+    } else if (((e.clientX - totalBar.offsetLeft) / totalBar.offsetWidth) * 100 < 0) {
+      progressBar.style.width = "0%";
+      timeSet(`0`, "current");
+    } else {
+      progressBar.style.width = `${((e.clientX - totalBar.offsetLeft) / totalBar.offsetWidth) * 100}%`;
+      timeSet(
+        `${Math.floor((((e.clientX - totalBar.offsetLeft) / totalBar.offsetWidth) * 100 * songAudio.duration) / 100)}`,
+        "current"
+      );
+    }
+  }
+});
 
-totalBar.addEventListener("mouseup", (e) => {
-  songAudio.currentTime = (songAudio.duration * (e.offsetX * 100)) / totalBar.clientWidth / 100;
-  mouseState = null;
-  clearInterval(moveBar);
+window.addEventListener("mouseup", (e) => {
+  if (mouseDown === true) {
+    mouseDown = false;
+    songAudio.currentTime = (songAudio.duration * (e.offsetX * 100)) / totalBar.clientWidth / 100;
+  }
 });
