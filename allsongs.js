5afc482c2e372faeac6ccfab2c562657fde6b9ce
KLEIN Jean-Daniel <kleinjeandaniel@gmail.com>
2023-12-02 16:56:40 +0100

    added new songs

--- allsongs.js
+++ allsongs.js
@@ -11,4 +11,16 @@ let allSongs = [
     img: "cover-2",
     src: "forest-lullaby-110624",
   },
+  {
+    name: "Howd You Do It",
+    artist: "Will Cullen",
+    img: "cover-3",
+    src: "Howd You Do It - Will Cullen",
+  },
+  {
+    name: "Lay Low",
+    artist: "Tiësto",
+    img: "cover-4",
+    src: "Tiësto - Lay Low (Official Music Video)",
+  },
 ];
